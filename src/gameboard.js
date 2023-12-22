a387599621c5f5d9fb6633735b20b4780311e54a
Ethan Hall <ethanwhall@icloud.com>
2023-12-22 15:05:15 -0500

    add createBoard and placeShip methods

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,44 @@
+// import Ship from "./ship";
+const Ship = require('./ship');
+
 class GameBoard {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.board = this.createBoard();
+    }
+
+    // [
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0]
+    // ]
+
+    createBoard() {
+        let array = [];
+        for (let i = 0; i < this.height; i++) {
+            array.push([]);
+        }
+        // const array = new Array(this.height).fill({hasShip: false, isHit: false});
+        for(let i = 0; i < array.length; i++) {
+            for (let j = 0; j < this.width; j++) {
+                array[i].push({hasShip: false, isHit: false});
+            }
+            // array[i] = new Array(this.width).fill({hasShip: false, isHit: false});
+        }
+        return array;
+    }
+
+    placeShip(vertCoord, horzCoord, length, angle) {
+        const ship = new Ship(length);
+        console.log('Helllooooo');
+        for (let i = horzCoord; i < length+horzCoord; i++) {
+            console.log('Helloooo inside for looooop');
+            console.log(this.board[vertCoord][i]);
+            console.log(i);
+            this.board[vertCoord][i] = ship;
+        }
     }
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,14 +2,21 @@ const GameBoard = require('../gameboard');
 
 let board;
 beforeEach(() => {
-    board = new GameBoard(100, 100);
+    board = new GameBoard(6, 6);
 
 })
 
-test('gameboard length and width work', () => {
-    expect(board.width && board.height).toBe(100);
+
+test('gameboard created properly', () => {
+    console.log(board.board);
+    expect(board.width && board.height).toBe(6);
+    expect(typeof(board.board)).toBe('object');
 })
 
 test('ship is placeable at specific coordinates', () => {
-
+    board.placeShip(5, 5, 'vert');
+    console.log(board.board);
+    console.log(board.board[5][5]);
+    expect(board.board[5][5].hasShip).toBe(true);
+    // expect(board.hasShip(5, 5)).toBe(true);
 })
