9e50bad6a025f40bfa65c5b9bb5667d15175c0c3
Ethan Hall <ethanwhall@icloud.com>
2023-12-28 23:16:42 -0500

    Add updateDisplay and attack input

--- src/dom-control.js
+++ src/dom-control.js
@@ -1,10 +1,22 @@
 class DomControl {
+    // constructor() {
+    //     this.playerBoardElem = document.querySelector('.player-board');
+    //     this.enemyBoardElem = document.querySelector('.enemy-board');
+    // }
+
+    static updateDisplay(pOneBoard, pTwoBoard) {
+        const playerBoardElem = document.querySelector('.player-board');
+        const enemyBoardElem = document.querySelector('.enemy-board');
+
+        playerBoardElem.textContent = '';
+        enemyBoardElem.textContent = '';
+
+        this.displayBoard(pOneBoard, playerBoardElem);
+        this.displayBoard(pTwoBoard, enemyBoardElem);
+    }
     
-    static displayBoard(gameBoard) {
+    static displayBoard(gameBoard, boardElem) {
         const boardCont = document.querySelector('.boards-cont');
-        const boardElem = document.createElement('div');
-        boardElem.classList.add('board');
-        boardCont.appendChild(boardElem);
         
         //create gameboard div, append to body
         const board = gameBoard.board;
@@ -24,13 +36,14 @@ class DomControl {
                 
             const cellElem = document.createElement('div');
             cellElem.classList.add('board-cell');
+            cellElem.setAttribute('data-coordinate', `[${i}, ${j}]`)
             console.log(boardElem.offsetWidth);
             cellElem.style.width = `${boardElem.clientWidth / gameBoard.width}px`;
             cellElem.style.height = `${boardElem.clientHeight / gameBoard.height}px`;
             
             this.checkShip(cellElem, gameBoard.board[i][j]);
-            // checkMiss();
-            // checkHit();
+            this.checkMiss(cellElem, gameBoard.board[i][j]);
+            this.checkHit(cellElem, gameBoard.board[i][j]);
             
             boardElem.appendChild(cellElem);
             }
@@ -49,8 +62,33 @@ class DomControl {
 
     static checkShip(cellElem, coordinate) {
         if (coordinate.ship !== false) {
+            cellElem.style.backgroundColor = 'blue';
+        }
+    }
+
+    static checkMiss(cellElem, coordinate) {
+        if (coordinate.isCellMissed === true) {
+            cellElem.style.backgroundColor = 'gray';
+        }
+    }
+
+    static checkHit(cellElem, coordinate) {
+        if (coordinate.isCellHit === true) {
             cellElem.style.backgroundColor = 'red';
         }
+    }
+
+    static inputAttack(playerBoard, enemyBoard) {
+        document.addEventListener('click', (event) => {
+            if (event.target.classList.contains('board-cell')) {
+                // console.log(event.target.getAttribute('data-coordinate'));
+                const coordinates = JSON.parse(event.target.getAttribute('data-coordinate'));
+                console.log(coordinates);
+                console.log(enemyBoard.board[coordinates[0], coordinates[1]]);
+                enemyBoard.receiveAttack(coordinates[0], coordinates[1]);
+                this.updateDisplay(playerBoard, enemyBoard);
+            }
+        })
     }
 }
 
--- src/game-loop.js
+++ src/game-loop.js
@@ -2,25 +2,68 @@ const Player = require('./player');
 const GameBoard = require('./gameboard');
 const DomControl = require('./dom-control');
 
+class Game {
+    constructor(pOneName, pTwoName, dimensions) {
+        this.pOneName = pOneName;
+        this.pTwoName = pTwoName;
+        this.dimensions = dimensions;
 
-function newGame() {
-    const pOneName = 'Bill';
-    const pTwoName = 'Sarah';
-    const dimensions = 10;
-    
-    const playerOne = new Player(pOneName);
-    const playerTwo = new Player(pTwoName);
-    
-    const pOneBoard = new GameBoard(dimensions, dimensions);
-    const pTwoBoard = new GameBoard(dimensions, dimensions);
+        this.playerOne - new Player(this.pOneName);
+        this.playerTwo = new Player(this.pTwoName);
 
-    pOneBoard.placeShip(4, 2, 4, 'horiz');
+        this.pOneBoard = new GameBoard(this.dimensions, this.dimensions)
+        this.pTwoBoard = new GameBoard(this.dimensions, this.dimensions)
+    }
 
-    DomControl.displayBoard(pOneBoard);
-    DomControl.displayBoard(pTwoBoard);
 }
 
-newGame();
+const game = new Game('Bill', 'Sarah', 10);
+
+game.pOneBoard.placeShip(4, 2, 4, 'horiz');
+game.pOneBoard.placeShip(1, 2, 2, 'vert');
+
+game.pTwoBoard.placeShip(5, 5, 4, 'horiz');
+
+game.pOneBoard.receiveAttack(7, 5);
+game.pTwoBoard.receiveAttack(2, 1);
+
+game.pOneBoard.receiveAttack(4, 2);
+
+DomControl.updateDisplay(game.pOneBoard, game.pTwoBoard);
+
+// DomControl.displayBoard(game.pOneBoard);
+// DomControl.displayBoard(game.pTwoBoard);
+
+DomControl.inputAttack(game.pOneBoard, game.pTwoBoard);
+
+// function newGame() {
+//     // const pOneName = 'Bill';
+//     // const pTwoName = 'Sarah';
+//     // const dimensions = 10;
+    
+//     // const playerOne = new Player(pOneName);
+//     // const playerTwo = new Player(pTwoName);
+    
+//     // const pOneBoard = new GameBoard(dimensions, dimensions);
+//     // const pTwoBoard = new GameBoard(dimensions, dimensions);
+
+//     pOneBoard.placeShip(4, 2, 4, 'horiz');
+//     pOneBoard.placeShip(1, 2, 2, 'vert');
+
+//     pTwoBoard.placeShip(5, 5, 4, 'horiz');
+
+//     pOneBoard.receiveAttack(7, 5);
+//     pTwoBoard.receiveAttack(2, 1);
+
+//     pOneBoard.receiveAttack(4, 2);
+
+//     DomControl.displayBoard(pOneBoard);
+//     DomControl.displayBoard(pTwoBoard);
+// }
+
+
+
+// newGame();
 
 
 
